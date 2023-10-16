e2b62a4981b1e89b18c828a9aec2a78a42f71746
mans5834 <105821977+mans5834@users.noreply.github.com>
2023-10-16 00:27:23 -0700

    introduction

---
+++ GeneralFunctions.hpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+//Start of function declarations
+int introduction();
+//End of function declarations
+
+int introduction(){   //explains intitial instructions for program use
+
+    int option = 0;
+
+    cout << "Hi, this program will help to decipher morse code that does not include breaks." << endl;
+    cout << "Please follow the instructions below to understand how to interact with the program." << endl;
+    cout << "\n 1. After these instructions you will be prompted to enter the word you are trying to decipher." << endl;
+    cout << " 2. The morse code will then be deciphered and you will be told what the word is or if the morse code does not represent a word in the English language." << endl;
+    cout << "\n\n Would you like to enter your morse code as 0's and 1's representing dots and dashes or use \".\" as a dot and \"-\" as a dash?" << endl;
+    cout << "Enter \"1\" for the first option and \"2\" for the second option." << endl;
+    cin >> option;
+
+    return option;
+
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "GeneralFunctions.hpp"
+
+
+int main(){
+
+    int chosenOption = introduction();
+    if(chosenOption == 1){
+        cout << "h" << endl;
+    }else{
+        cout << "z" << endl;
+    }
+
+}
